45428f6127f23dfbc49cb85bda5f27d748163980
Le Duy Bach <ldbach@apcs.vn>
2018-04-23 10:17:38 +0700

    Try implement loading list using Observable instead of Single

--- app/src/main/java/com/example/cpu02351_local/rxjavaexample/MainActivity.kt
+++ app/src/main/java/com/example/cpu02351_local/rxjavaexample/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.cpu02351_local.rxjavaexample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.widget.ArrayAdapter
 import com.example.cpu02351_local.retrofitdemo.GithubClient
 import com.example.cpu02351_local.retrofitdemo.GithubRepo
@@ -13,8 +12,6 @@ import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.OkHttpClient
 import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -28,33 +25,40 @@ class MainActivity : AppCompatActivity() {
 
     private fun loadDataAndFillList() {
         val call = getApiCallable()
-        val callGithubApi = createRxSingle(call)
-        setupAndSubscribeToRxSingle(callGithubApi)
+        val callGithubApi = createRxObservable(call)
+        setupAndSubscribeToRxObservable(callGithubApi)
     }
 
-    private fun setupAndSubscribeToRxSingle(callGithubApi: Single<List<GithubRepo>>?) {
+    private fun setupAndSubscribeToRxObservable(callGithubApi: Observable<GithubRepo>?) {
         callGithubApi!!
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(object : SingleObserver<List<GithubRepo>> {
+                .subscribe(object : Observer<GithubRepo> {
+                    var adapter: ArrayAdapter<GithubRepo>? = null
+                    var list = ArrayList<GithubRepo>()
                     var d: Disposable? = null
 
-                    override fun onSuccess(list: List<GithubRepo>) {
-                        // Inflate ListView
-                        val adapter =
-                                ArrayAdapter<GithubRepo>(
-                                        this@MainActivity,
-                                        R.layout.list_item,
-                                        list)
-                        list_item.adapter = adapter
 
+                    override fun onComplete() {
                         if (d != null && !d!!.isDisposed)
                             d!!.dispose()
+                    }
+
+                    override fun onNext(t: GithubRepo) {
+                        list.add(t)
+                        adapter?.notifyDataSetChanged()
                     }
 
                     override fun onSubscribe(d: Disposable) {
                         // Meh?
                         this.d = d
+                        // Inflate ListView
+                        adapter =
+                                ArrayAdapter(
+                                        this@MainActivity,
+                                        R.layout.list_item,
+                                        list)
+                        list_item.adapter = adapter
                     }
 
                     override fun onError(e: Throwable) {
@@ -63,12 +67,15 @@ class MainActivity : AppCompatActivity() {
                 })
     }
 
-    private fun createRxSingle(call: Call<List<GithubRepo>>): Single<List<GithubRepo>>? {
-        return Single.create { emitter ->
+    private fun createRxObservable(call: Call<List<GithubRepo>>):  Observable<GithubRepo>? {
+        return Observable.create { emitter ->
 
             val response = call.execute()
-            response.body()?.let { emitter.onSuccess(it) }
-                    ?: emitter.onError(Exception("Fail to get result"))
+            for (repo in response.body()!!) {
+                Thread.sleep(100)
+                emitter.onNext(repo)
+            }
+            emitter.onComplete()
 
             /*
             call.enqueue(object : Callback<List<GithubRepo>> {
